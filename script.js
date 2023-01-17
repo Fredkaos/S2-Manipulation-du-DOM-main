fcac83c463f5bf74a152f65e3a1d0a08e37dd3f1
1079058 <1079058@cegepoutaouais.qc.ca>
2023-01-17 13:13:08 -0500

    deuxieme

--- script.js
+++ script.js
@@ -9,23 +9,32 @@ function ajouter(choix)
     {
         choix1 = "qtePommes";
         choix2 = "poidsPommes";
-        choix3 ="prixPommes";
+        _poids = 10;
+        choix3 = "prixPommes";
+        _prix = 13.50;
     }
     else if (choix === 'poires')
     {
-        let qte = document.getElementById("qtePoires").innerText;
-        qte = +qte + 1;
-        document.getElementById("qtePoires").innerText = qte;
+        choix1 = "qtePoires";
+        choix2 = "poidsPoires";
+        _poids = 12;
+        choix3 = "prixPoires";
+        _prix = 20.00;
     }
     else
     {
-        let qte = document.getElementById("qtePrunes").innerText;
-        qte = +qte + 1;
-        document.getElementById("qtePrunes").innerText = qte;
+        choix1 = "qtePrunes";
+        choix2 = "poidsPrunes";
+        _poids = 15;
+        choix3 = "prixPrunes";
+        _prix = 22.00;
     }
     let qte = document.getElementById(choix1).innerText;
     qte = +qte + 1;
     document.getElementById(choix1).innerText = qte;
+    let poid = document.getElementById(choix2).innerText;
+    poid = +poid + _poids;
+    document.getElementById(choix2).innerText = poid;
 }
 // Pour afficher le nombre de pommes dans la facture détaillée
 document.querySelector("#qtePommes").textContent = panier.pommes;
